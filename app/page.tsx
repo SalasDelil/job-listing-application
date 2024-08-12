105fabf7e373bd8e52a18a1dcadbe1fea39cfd94
SalasDelil <salsdelil8@gmail.com>
2024-08-12 10:37:04 +0300

    Initial commit.

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+// useful tawilwindcss clases
+// object-cover
+// overflow-hidden
+// upercase
+// rounded
+// absolute
+// relative
+// top-0
+// @apply directive - for reusing applied styles for different components
+// grid gird-cols-3
+// gap-4
+// col-span-1 and col-span-2 for the other\
+// transition ease- duration-
+export default function Home() {
+  return (
+    <main className="">
+      <h1>Jobs Portal</h1>
+    </main>
+  );
+}
